e657b6469748ebe3e9be21b8db3f35b0c1d61b5c
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-05 02:02:12 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,7 @@ def remove_strawberry(contacts)
     data.each do |attribute, value|
       if attribute == :favorite_ice_cream_flavors
         value.each do |flavor|
-          flavor.shift {|a| a = "strawberry"}
+          flavor.delete("strawberry")
         end
       end
     end
